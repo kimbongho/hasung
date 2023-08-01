a92c92994518cae44638df4de1435acbb74f95be
kimbongho <31979850+kimbongho@users.noreply.github.com>
2023-08-01 18:56:43 +0900

    123123

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,29 @@
     <!-- About Start -->
     <div class="container-xxl py-5">
         <div class="container">
-            회사소개...
+            <div class="row g-5">
+                <div class="col-lg-5 wow fadeIn" data-wow-delay="0.1s">
+                    <img class="img-fluid" src="@/assets/images/article.jpg" alt="">
+                </div>
+                <div class="col-lg-6 wow fadeIn" data-wow-delay="0.5s">
+                    <div class="section-title">
+                        <p class="fs-5 fw-medium fst-italic text-primary">회사소개</p>
+                        <h1 class="display-6">항상 신속, 정확, 안전하게 서비스를 제공하겠습니다.</h1>
+                    </div>
+                    <p class="mb-4">
+                        한성응급환자이송단에서는 24시 언제나 응급출동을 할 수 있도록 특수 구급차 및 일반 구급차와 응급구조사 의료진이 대기하고 있습니다.
+                        언제나 환자의 생명을 소중히 여기며 병원으로 환자를 이송할 때 구급차 안에서 응급 처치가 가능하도록 응급의료장비를 최상으로 장차갛여 응급환자 이송 시 구급원과
+                        응급 의료진이 함께 응급출동하며 구급차내에서 어떠한 위급상황이 와도 응급 처치가 가능하며 신속하게 병원으로 이송하여 드리고 있습니다.
+                    </p>
+                    <p class="mb-4">
+                        또한 특수 구급차에 벤트레이터(인공호흡기)를 장착하여 환자 이송시 2차 사고를 방지하고 있습니다.
+                        앞으로도 한성응급화자이송단에서는 환자의 생명을 최우선으로 하는 선진국 의료 서비스로 24시 언제나 응급 출동이 가능하도록 
+                        항상 준비된 자세와 환자를 생각하는 마음으로 노력해 나아가겠습니다.<br/>
+                        감사합니다.
+                    </p>
+                    <!-- <a href="" class="btn btn-primary rounded-pill py-3 px-5">Read More</a> -->
+                </div>
+            </div>
         </div>
     </div>
     <!-- About End -->
