a299c3bfa9a36d5d28fc83b552b0470d59b70d16
kimbongho <31979850+kimbongho@users.noreply.github.com>
2023-08-03 11:43:52 +0900

    sdfsdfsdfsdf

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import '@/assets/css/bootstrap.min.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import '@/assets/lib/animate.css'
+import 'owl.carousel/dist/assets/owl.carousel.css'
+import 'owl.carousel'
 import { createApp } from 'vue'
 import App from './App.vue'
 import store from '@/store'
